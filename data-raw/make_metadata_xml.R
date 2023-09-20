041faedb0b1dadf269815229ea0731de1f3b780c
liz-fw <lstebbins@flowwest.com>
2023-09-20 13:13:52 -0700

    uploads package to edi

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -8,20 +8,20 @@ library(EMLaide)
 datatable_metadata <-
   dplyr::tibble(filepath = c("data/delta_entry_catch_edi.csv",
                              "data/delta_entry_trap_edi.csv",
-                             "data/delta_entry_recapture_edi.csv",
+                             ##"data/delta_entry_recapture_edi.csv",
                              "data/delta_entry_release_edi.csv"),
                 attribute_info = c("data-raw/metadata/delta_entry_catch_metadata.xlsx",
                                    "data-raw/metadata/delta_entry_trap_metadata.xlsx",
-                                   "data-raw/metadata/delta_entry_recapture_metadata.xlsx",
+                                   #"data-raw/metadata/delta_entry_recapture_metadata.xlsx",
                                    "data-raw/metadata/delta_entry_release_metadata.xlsx"),
                 datatable_description = c("Daily catch",
                                           "Daily trap operations",
-                                          "Recaptured fish",
+                                          #"Recaptured fish",
                                           "Released fish"),
                 datatable_url = paste0("https://raw.githubusercontent.com/SRJPE/jpe-delta-entry-edi/main/data/",
                                        c("delta_entry_catch_edi.csv",
                                          "delta_entry_trap_edi.csv",
-                                         "delta_entry_recapture_edi.csv",
+                                         #"delta_entry_recapture_edi.csv",
                                          "delta_entry_release_edi.csv")))
 
 excel_path <- "data-raw/metadata/delta_entry_metadata.xlsx"
